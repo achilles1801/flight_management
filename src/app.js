acc312bfc367422eea7c147d68e9d69449f45394
Majd Khawaldeh <majd@gatech.edu>
2023-12-27 10:53:22 -0500

    auth0 and jwt

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlHTTP } from 'express-graphql';
 import schema from './graphql/schema.js';
+import pkg from 'express-openid-connect';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
 
 import airline from './routes/airline.js';
 import location from './routes/location.js';
@@ -17,10 +21,52 @@ import flight from './routes/flight.js';
 import pilot from  './routes/pilot.js';
 import pilot_licenses from './routes/pilot_licenses.js';
 
+
+
+dotenv.config();
+
 const app = express();
 const PORT = 3000;
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: 'http://localhost:3000',
+  clientID: 'xVHyHjLXkehoNDEDJ9a8jiIezgRJU7cU',
+  issuerBaseURL: 'https://dev-l38tsgz4skl46fcs.us.auth0.com'
+};
+
+const { auth, requiresAuth } = pkg;
 app.use(bodyParser.json());
+app.use(auth(config));
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+// Middleware to validate JWT for GraphQL mutations
+const validateJwtForMutations = (req, res, next) => {
+  if (req.body && req.body.query && req.body.query.includes('mutation')) {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+      return res.status(401).send('Access Denied: No Token Provided!');
+    }
+    try {
+      const verified = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = verified;
+    } catch (error) {
+      return res.status(400).send('Invalid Token');
+    }
+  }
+  next();
+};
+
+
 
 app.use('/',
   airline,
@@ -36,13 +82,12 @@ app.use('/',
   pilot,
   pilot_licenses
   );
-
-  
-// Set up GraphQL endpoint
-app.use('/graphql', graphqlHTTP({
+// Set up GraphQL endpoint with JWT validation for mutations
+app.use('/graphql', validateJwtForMutations, graphqlHTTP((req) => ({
   schema: schema,
   graphiql: true, // Set to false in production
-}));
+  context: { user: req.user } // Pass user info to GraphQL context
+})));
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
   });
--- src/graphql/resolvers/airlineResolver.js
+++ src/graphql/resolvers/airlineResolver.js
@@ -23,6 +23,10 @@ export const AirlineQuery = {
   // ...other queries
 };
 
+const checkIfAuthorized = (user) => {
+  return user && user.role === 'admin';
+};
+
 export const AirlineMutation = {
   addAirline: {
     type: AirlineType,
@@ -30,8 +34,11 @@ export const AirlineMutation = {
       airlineID: { type: GraphQLString },
       revenue: { type: GraphQLInt }
     },
-    resolve(parent, args) {
-      // Replace with actual database query
+    resolve(parent, args, context) {
+      // Check if user is authorized
+      if (!checkIfAuthorized(context.user)) {
+        throw new Error('Not authorized!');
+      }
       return new Promise((resolve, reject) => {
         db.query('INSERT INTO airline (airlineID, revenue) VALUES (?, ?)', 
                  [args.airlineID, args.revenue], (error, results) => {
